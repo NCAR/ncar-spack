e36adbd7c3e2a570ddd66aa8393fd124b283f479
Brian Vanderwende <vanderwb@ucar.edu>
2022-05-03 16:22:12 -0600

    Improvements for crayenv postprocessing

--- src/env/bin/tools/init.sh
+++ src/env/bin/tools/init.sh
@@ -16,22 +16,23 @@ elif [[ -z $SPACK_ENV ]]; then
     set -e
     my_name=$(basename "$0")
     my_host=$(hostname)
+    my_env_type=${NCAR_SPACK_ENV_TYPE:-build}
 
-    tsecho "Activating Spack build environment"
+    tsecho "Activating Spack $my_env_type environment"
     
-    if [[ -f $my_dir/../spack.yaml ]]; then
-        spack env activate $my_dir/..
+    if [[ -f $NCAR_SPACK_ENV_ROOT/$my_env_type/spack.yaml ]]; then
+        spack env activate $NCAR_SPACK_ENV_ROOT/$my_env_type
     else
         echo "Error:  This $my_name script does not appear to be part of a cluster"
-        echo -e "        build environment. Use \$SPACK_ENV/bin/$my_name instead.\n"
+        echo -e "        $my_env_type environment. Use \$SPACK_ENV/bin/$my_name instead.\n"
         exit 1
     fi
 
-    tsecho "Testing whether user is owner of build env"
+    tsecho "Testing whether user is owner of $my_env_type env"
     spack_env_user=$(stat -c "%U" $SPACK_ENV)
 
     if [[ $USER != $spack_env_user ]]; then
-        echo "Error:  This script must be run by the owner of the active build environment."
+        echo "Error:  This script must be run by the owner of the active $my_env_type environment."
         echo -e "        $SPACK_ENV -> owned by $spack_env_user"
         exit 1
     fi
