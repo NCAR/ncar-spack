a72ee8b120519d45b757f5e77ba7a897e2aab9dd
Brian Vanderwende <vanderwb@ucar.edu>
2022-08-25 22:46:14 -0600

    Change full builtin for limited future repo

--- repos/future/setup.sh
+++ repos/future/setup.sh
@@ -1,9 +1,8 @@
 # *** This script should be sourced by deploy only! ***
 #
-# NOTE -    this repo *must* be called builtin as it replaces the
-#           packaged builtin repo in the main spack clone. Some
-#           packages reference builtin explicitly and so concretization
-#           will fail otherwise!
+# This repo provides a sparse clone of builtin, but it does not replace
+# the built-in builtin! Use it to selectively add newer versions of package
+# recipes by symbolically linking them into the packages directory.
 
 return_dir=$(pwd)
 
@@ -12,14 +11,13 @@ git clone -c feature.manyFiles=true --sparse --filter blob:none https://github.c
 cd $repo
 git sparse-checkout set var/spack/repos/builtin/packages
 git checkout ${NCAR_SPACK_BUILTIN_VERSION:-$NCAR_SPACK_CLONE_VERSION}
-ln -s var/spack/repos/builtin/packages packages
+mkdir packages
 cat > repo.yaml << EOF
 repo:
-  namespace: 'builtin'
+  namespace: 'future'
 EOF
 
-# Make sure the repo *replaces* the standard builtin
+# Use this repo in the environment
 spack repo add $SPACK_ENV/repos/$repo
-sed -i 's/\([ ]*repos:\)$/\1:/' $SPACK_ENV/spack.yaml 
 
 cd $return_dir
