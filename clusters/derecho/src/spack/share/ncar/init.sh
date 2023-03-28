e72f14475283e7f5984d84b9e5f341562d327122
Brian Vanderwende <vanderwb@ucar.edu>
2023-03-27 23:09:47 -0600

    Begin unifying gust and derecho recipes

--- clusters/derecho/src/spack/share/ncar/init.sh
+++ clusters/derecho/src/spack/share/ncar/init.sh
@@ -2,10 +2,13 @@
 # we are working on a clean system or not!
 if [[ -f /etc/bash.bashrc.local ]]; then
     . /etc/bash.bashrc.local
-elif type -t module >& /dev/null; then
-    . /etc/profile.d/z00_modules.sh
-    module --force purge
-    module load crayenv
+elif [[ -f /etc/profile.d/z00_modules.sh ]]; then
+    . /etc/profile.d/z00_modules.sh 2> /dev/null
+
+    if module -v 2> /dev/null; then
+        module --force purge
+        module load crayenv
+    fi
 fi
 
 # If left set, will contaminate Spack child shells
