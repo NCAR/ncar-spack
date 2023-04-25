03e3278dec6148157eb0f90009c05209629a8961
Brian Vanderwende <vanderwb@ucar.edu>
2023-04-25 14:09:17 -0600

    Wrapper module fix for Intel permutations

--- templates/modules/ncar_wrapper.lua
+++ templates/modules/ncar_wrapper.lua
@@ -4,4 +4,17 @@
 if os.getenv("LMOD_FAMILY_MPI") then
     prepend_path("PATH", pathJoin("{{spec.prefix.bin}}", "mpi"))
 end
+
+-- Special logic for intel modules
+local comp_name = os.getenv("LMOD_FAMILY_COMPILER")
+
+if (comp_name == "intel") then
+    setenv("FC",    "ifort")
+    setenv("F77",   "ifort")
+elseif (comp_name == "intel-classic") then
+    setenv("CC",    "icc")
+    setenv("CXX",   "icpc")
+    setenv("FC",    "ifort")
+    setenv("F77",   "ifort")
+end
 {% endblock %}
