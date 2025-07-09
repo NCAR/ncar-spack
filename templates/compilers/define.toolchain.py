f6954ecae03e3393454184e82ae7975c6453baf4
Brian Vanderwende <vanderwb@ucar.edu>
2025-07-08 22:59:23 -0600

    Add lapack to toolchain and create amd casper cluster

--- templates/compilers/define.toolchain.py
+++ templates/compilers/define.toolchain.py
@@ -23,15 +23,19 @@ def deep_merge(p: dict, a: dict):
 num_args = len(sys.argv)
 
 if num_args < 3:
-    raise TypeError("Usage: define.toolchain.py TOOLCHAIN COMPILER [BLAS]")
+    raise TypeError("Usage: define.toolchain.py TOOLCHAIN COMPILER [BLAS] [LAPACK]")
 else:
     toolchain = sys.argv[1]
     comp_spec = sys.argv[2]
 
-    if num_args == 4:
+    if num_args > 3:
         blas_spec = sys.argv[3]
+        lapack_spec = sys.argv[3]
     else:
         blas_spec = None
+
+    if num_args == 5:
+        lapack_spec = sys.argv[4]
 
 #
 ## Environment Modifications
@@ -41,7 +45,7 @@ virtuals_list = []
 
 for virtual in ("c", "cxx", "fortran", "blas", "lapack"):
     if virtual in ("blas", "lapack") and blas_spec:
-        virtuals_list.append(CommentedMap({ "spec" : f"%{virtual}={blas_spec}",
+        virtuals_list.append(CommentedMap({ "spec" : f"%{virtual}={blas_spec if virtual == 'blas' else lapack_spec}",
                                             "when" : f"%{virtual}"}))
     else:
         virtuals_list.append(CommentedMap({ "spec" : f"%{virtual}={comp_spec}",
