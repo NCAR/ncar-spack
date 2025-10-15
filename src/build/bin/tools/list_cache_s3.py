2890a334b27a69327a962dbc139d9f1c58a72a58
Brian Vanderwende <vanderwb@ucar.edu>
2025-10-15 10:17:26 -0600

    Pull in updates from casper-amd work

--- src/build/bin/tools/list_cache_s3.py
+++ src/build/bin/tools/list_cache_s3.py
@@ -1,12 +1,25 @@
-import boto3, sys
-
-# Log into the S3 server
-session = boto3.Session()
-s3 = session.resource('s3')
+import boto3, sys, re
 
 # Get the bucket name
-bucket_name = sys.argv[1]
+try:
+    bucket_name = sys.argv[1]
+except KeyError:
+    print("Usage: spack python list_cache_s3.py BUCKET [PROFILE]")
+    raise
+
+if len(sys.argv) == 3:
+    s3_profile = sys.argv[2]
+    session = boto3.Session(profile_name = s3_profile)
+else:
+    session = boto3.Session()
+
+# Log into the S3 server
+s3 = session.resource('s3')
 cache_bucket = s3.Bucket(bucket_name)
 
-for objects in cache_bucket.objects.filter(Prefix="build_cache/"):
-    print(objects.key)
+for obj in cache_bucket.objects.filter(Prefix = "v3/manifests/spec"):
+    if obj.key.endswith(".spec.manifest.json"):
+        try:
+            print(re.search(r".*/(.*).spec.manifest.json", obj.key).group(1))
+        except:
+            pass
