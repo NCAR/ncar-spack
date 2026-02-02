e0ff2050fb1baa84abd2280f7d57ba4f3938615d
Brian Vanderwende <vanderwb@ucar.edu>
2026-02-02 10:28:39 -0700

    Improved registry design and debug pkg support

--- src/build/bin/tools/init.sh
+++ src/build/bin/tools/init.sh
@@ -50,32 +50,118 @@ for cfg_var in ${!NCAR_SPACK_*}; do
     export $cfg_var
 done
 
+#
+## SHARED REGISTRY CONFIGURATION
+#
+
 # The following variables are shared among multiple scripts
 # Note that shell arrays can't be exported, so we must define it here to be
 # reinstantiated for each script
-declare -A field_vars field_labels
-field_widths=( 32 19 40 4 6 7 8 9 )
-field_vars[32]=spec_hash
-field_vars[19]=install_date
-field_vars[40]=spack_commit
-field_vars[4]="config[maxjobs]"
-field_vars[6]="config[trust]"
-field_vars[7]="config[cache]"
-field_vars[8]="config[publish]"
-field_vars[9]="config[register]"
-field_labels[spec_hash]="Package Hash"
-field_labels[install_date]="Date Installed"
-field_labels[spack_commit]="Spack Git Repo Commit"
-field_labels["config[maxjobs]"]="Jobs"
-field_labels["config[trust]"]="Trust?"
-field_labels["config[cache]"]="Cached?"
-field_labels["config[publish]"]="Publish?"
-field_labels["config[register]"]="Register?"
+declare -A field_widths field_labels registry_entries
+field_list=(    spec_hash install_date spack_commit "config[maxjobs]" "config[trust]"
+                "config[cache]" "config[publish]" "config[register]" "config[source]"
+                raw_spec spec   )
 
-install_params="cache|trust|maxjobs|access|register"
+field_widths[spec_hash]=32              field_labels[spec_hash]="Package Hash"
+field_widths[install_date]=19           field_labels[install_date]="Date Installed"
+field_widths[spack_commit]=40           field_labels[spack_commit]="Spack Git Repo Commit"
+field_widths["config[maxjobs]"]=4       field_labels["config[maxjobs]"]="Jobs"
+field_widths["config[trust]"]=6         field_labels["config[trust]"]="Trust?"
+field_widths["config[cache]"]=7         field_labels["config[cache]"]="Cached?"
+field_widths["config[publish]"]=8       field_labels["config[publish]"]="Publish?"
+field_widths["config[register]"]=9      field_labels["config[register]"]="Register?"
+field_widths["config[source]"]=10       field_labels["config[source]"]="Keep Src?"
+field_widths[raw_spec]=*                field_labels[raw_spec]="Raw Spec"
+field_widths[spec]=                     field_labels[spec]="Spec"
+
+install_params="cache|trust|maxjobs|access|register|source"
 default_trust=no
 default_cache=yes
 default_publish=yes
+default_source=no
+
+function add_spec_to_registry {
+    IFS='|' read -a registry <<< $1
+    duplicate_value=${registry_entries[${registry[0]}]}
+    unset registry_entries["${registry[0]}"]
+
+    for n in ${!field_list[@]}; do
+        if [[ ${field_widths[${field_list[n]}]} == \* ]]; then
+            width=${spec_column_width:-$(($(wc -c <<< ${registry[$n]}) - 1))}
+
+            if [[ $width -lt 20 ]]; then
+                width=20
+            fi
+        fi
+
+        if [[ $n -gt 0 ]]; then
+            registry_entries[${registry[0]}]+=" | "
+        fi
+
+        registry_entries[${registry[0]}]+=$(printf "%-${field_widths[${field_list[n]}]}s" $width "${registry[$n]}")
+        unset width
+    done
+
+    if [[ -n $duplicate_value ]]; then
+        if [[ $duplicate_value == ${registry_entries[${registry[0]}]} ]]; then
+            tsecho " >> Duplicate registry entry for /${registry[0]}; skipping"
+            return
+        else
+            tsecho "Error: Two inconsistent registry entries for /${registry[0]}; cannot continue!"
+            exit 1
+        fi
+    fi
+
+    echo "${registry_entries[${registry[0]}]}" >> $2
+}
+
+function write_registry_header {
+    rm -f $SPACK_ENV/.registry.tmp
+    dash_width=0
+
+    for var in ${field_list[@]}; do
+        header="${header:+${header}|}${field_labels[$var]}"
+
+        if [[ ${field_widths[$var]} == \* ]]; then
+            dash_width=$((dash_width + ${spec_column_width:-20} + 3))
+        else
+            dash_width=$((dash_width + ${field_widths[$var]:-20} + 3))
+        fi
+    done
+
+    add_spec_to_registry "$header" $SPACK_ENV/.registry.tmp
+    printf "%*s\n" $dash_width | tr " " "-" >> $SPACK_ENV/.registry.tmp
+}
+
+function find_var_from_label {
+    stripped_label=$(xargs <<< $1)
+
+    for var in ${!field_labels[@]}; do
+        if [[ ${field_labels[$var]} == $stripped_label ]]; then
+            echo $var
+            return
+        fi
+    done
+
+    tsecho "Error: Existing label '$stripped_label' not found in registry configuration!"
+    exit 1
+}
+
+function match_spec_column_width {
+    IFS='|' read -a header_labels <<< $(head -n 1 $1)
+
+    for n in ${!field_list[@]}; do
+        if [[ ${field_list[$n]} == raw_spec ]]; then
+            spec_index=$n
+        fi
+    done
+
+    spec_column_width=$(($(wc -c <<< ${header_labels[$spec_index]}) - 3))
+}
+
+#
+## SPACK ENVIRONMENT SETUP
+#
 
 # Calling Python is much faster than calling spack python but needs some setup
 pypath="$SPACK_ROOT/lib/spack/external:$SPACK_ROOT/lib/spack/external/_vendoring:$SPACK_ROOT/lib/spack"
@@ -100,7 +186,7 @@ elif [[ -z $SPACK_ENV ]]; then
     export start_time=${NCAR_SPACK_DEPLOY_TIME:-$(date +%y%m%dT%H%M)}
 
     tsecho "Activating Spack $my_env_type environment" ${quiet_mode:+9999}
-    
+
     if [[ -f $NCAR_SPACK_ROOT_ENVS/$my_env_type/spack.yaml ]]; then
         spack env activate $NCAR_SPACK_ROOT_ENVS/$my_env_type
     else
